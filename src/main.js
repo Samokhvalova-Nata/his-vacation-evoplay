68f731872af5f266e9936effcfce24689c4b2a91
TatianaSkl <sklyarchyk@gmail.com>
2024-06-26 13:23:28 +0300

    fixed js

--- src/main.js
+++ src/main.js
@@ -29,28 +29,4 @@
       }
     });
   });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-      slidesPerView: 2,
-      loop: true,
-      centeredSlides: true,
-      spaceBetween: 8,
-      grabCursor: true,
-      breakpoints: {
-        380: {
-          slidesPerView: 2.99,
-          centeredSlides: true,
-        },
-        1440: {
-          slidesPerView: 5,
-          spaceBetween: 32,
-        },
-      },
-      navigation: {
-        nextEl: '.btn-next',
-        prevEl: '.btn-prev',
-      },
-    });
-  });
 })();
